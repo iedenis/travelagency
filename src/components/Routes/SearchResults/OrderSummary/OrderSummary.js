8f1c105a882a92d4144da483a5a28b06653b5eb0
iedenis <denis.ievlev@gmail.com>
2020-02-08 05:46:04 +0200

    fixing bug on mobile

--- src/components/Routes/SearchResults/OrderSummary/OrderSummary.js
+++ src/components/Routes/SearchResults/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { TextField, Paper, Typography, Grid, Divider, MenuItem } from '@material-ui/core';
+import { TextField, Paper, Typography, Grid, Divider, MenuItem, useTheme, useMediaQuery } from '@material-ui/core';
 import styled from 'styled-components';
 
 const DriverDetails = styled(Paper)`
@@ -8,6 +8,7 @@ display: flex;
 justify-content: center;
 `
 const OrderSummary = ({ driver: { driver, setDriver } }, driverAge) => {
+    const ispc = useMediaQuery(useTheme().breakpoints.up('sm'));
 
     const [tempDriver, setTempDriver] = useState({
         title: '',
@@ -24,85 +25,93 @@ const OrderSummary = ({ driver: { driver, setDriver } }, driverAge) => {
 
     useEffect(() => {
         console.log(tempDriver);
-    }, [tempDriver])
+        console.log(ispc);
+    }, [tempDriver, ispc])
     return (
         <>
-        <DriverDetails>
+            <DriverDetails>
 
-            <form style={{ width: '70%', padding: '20px', border: '1px solid rgba(0, 0, 0, 0.12)', borderRadius: '4px' }} action="" noValidate>
-                <Grid container direction='column'>
-                    <Grid item><Typography align='center' variant='h6'>Driver details</Typography></Grid>
-                    <Divider />
-                    <Grid item lg={3} md={3} sm={3} xs={12}>
+                <form style={{ width: '70%', padding: '20px', border: '1px solid rgba(0, 0, 0, 0.12)', borderRadius: '4px' }} action="" noValidate>
+                    <Grid container direction='column'>
+                        <Grid item><Typography align='center' variant='h6'>Driver details</Typography></Grid>
+                        <Divider />
+                        <Grid item lg={3} md={3} sm={3} xs={12}>
 
-                        <TextField
-                            fullWidth
-                            id="title"
-                            select
-                            label="Title"
-                            value={driver.title}
-                        // defaultValue='Please select'
-                        // onChange={(event) => handleChange(event, 'title')}
-                        >
-                            <MenuItem value='Mr'>Mr</MenuItem>
-                            <MenuItem value='Mrs'>Mrs</MenuItem>
-                            <MenuItem value='Mis'>MISS</MenuItem>
-                            <MenuItem value='Dr'>Dr</MenuItem>
-                            <MenuItem value='Ms'>Ms</MenuItem>
-                        </TextField>
-                    </Grid>
+                            <TextField
+                                fullWidth
+                                id="title"
+                                select
+                                label="Title"
+                                value={driver.title}
+                            // defaultValue='Please select'
+                            // onChange={(event) => handleChange(event, 'title')}
+                            >
+                                <MenuItem value='Mr'>Mr</MenuItem>
+                                <MenuItem value='Mrs'>Mrs</MenuItem>
+                                <MenuItem value='Mis'>MISS</MenuItem>
+                                <MenuItem value='Dr'>Dr</MenuItem>
+                                <MenuItem value='Ms'>Ms</MenuItem>
+                            </TextField>
+                        </Grid>
 
-                    <Grid item sm style={{ display: 'flex' }}>
-                        <Grid container>
-                            <Grid style={{ marginRight: '20px' }} item sm >
-                                <TextField
-                                    fullWidth
-                                    required
-                                    id="first-name"
-                                    label="First Name"
-                                    name="firstName"
-                                    value={tempDriver.firstName}
-                                    onChange={event => handleChange(event)}
-                                />
-                            </Grid>
-                            <Grid item sm >
-                                <TextField
-                                    fullWidth
-                                    required
-                                    id="last-name"
-                                    label="Last Name"
-                                    name='lastName'
-                                    onChange={event => handleChange(event, 'lastname')}
-                                />
+                        <Grid item sm xs={12} style={{ display: 'flex' }}>
+                            <Grid container>
+                                <Grid item sm xs={12}
+                                    style={{ marginRight: ispc ? '20px' : '0px' }}  >
+                                    <TextField
+                                        fullWidth
+                                        required
+                                        id="first-name"
+                                        label="First Name"
+                                        name="firstName"
+                                        value={tempDriver.firstName}
+                                        onChange={event => handleChange(event)}
+                                    />
+                                </Grid>
+                                <Grid item sm xs={12} >
+                                    <TextField
+                                        fullWidth
+                                        required
+                                        id="last-name"
+                                        label="Last Name"
+                                        name='lastName'
+                                        onChange={event => handleChange(event, 'lastname')}
+                                    />
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
 
-                    <Grid item xs={12}>
-                        <TextField fullWidth required id="email-address" label="Email address" defaultValue="" />
-                    </Grid>
+                        <Grid item xs={12}>
+                            <TextField fullWidth required id="email-address" label="Email address" defaultValue="" />
+                        </Grid>
 
-                    <Grid item>
-                        <Grid container>
-                            <Grid item sm style={{ marginRight: '20px' }}><TextField fullWidth required id="phone" label="Phone" defaultValue="" /></Grid>
-                            <Grid item sm>
-                                <TextField
-                                    id="driver-age"
-                                    label="Driver age"
-                                    type="number"
-                                    defaultValue={driver.age}
-                                // onChange={event => setAge(event.target.value)}
-                                />
+                        <Grid item >
+                            <Grid container>
+                                <Grid item sm xs={12}lg={6} style={{ marginRight: ispc ? '20px' : '0px' }}>
+                                    <TextField
+                                        fullWidth
+                                        required id="phone"
+                                        label="Phone"
+                                        defaultValue="" />
+                                </Grid>
+                                <Grid item sm lg={2}>
+                                    <TextField
+                                        id="driver-age"
+                                        label="Driver age"
+                                        type="number"
+                                        defaultValue={driver.age}
+                                    // onChange={event => setAge(event.target.value)}
+                                    />
+                                </Grid>
                             </Grid>
                         </Grid>
+
                     </Grid>
 
-                </Grid>
+                </form>
+                <Divider />
 
-            </form>
-            <Divider />
-
-        </DriverDetails>
+            </DriverDetails>
         </>
 
     )
